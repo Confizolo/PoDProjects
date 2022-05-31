9fa929a343d2ba0792bb79b4d0c1b8d9b3e321b7
Filippo Conforto <filippo.conforto@outlook.com>
2022-05-31 10:38:31 +0000

    Solvent minimization

--- MS/src/scripts/minimize.sh
+++ MS/src/scripts/minimize.sh
@@ -2,6 +2,6 @@
 
 kind="solv_oct"
 
-sander -O -i minimize.in -o ../libs/1fk9_min_${kind}.out -p ../libs/1fk9_${kind}.prmtop -c ../libs/1fk9_${kind}.rst7  -r ../libs/1fk9_min_${kind}.ncrst
+mpirun -np 7 sander -O -i minimize.in -o ../libs/1fk9_min_${kind}.out -p ../libs/1fk9_${kind}.prmtop -c ../libs/1fk9_${kind}.rst7  -r ../libs/1fk9_min_${kind}.ncrst
 conda activate AmberTools21
 ambpdb -p ../libs/1fk9_${kind}.prmtop -c ../libs/1fk9_min_${kind}.ncrst > ../libs/1fk9_min_${kind}.pdb
