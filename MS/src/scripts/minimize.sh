38d30c59e789ef5958a32d764548588d4bff9580
Filippo Conforto <filippo.conforto@outlook.com>
2022-06-09 13:38:48 +0000

    minimization of some

--- MS/src/scripts/minimize.sh
+++ MS/src/scripts/minimize.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-kind="solv_oct"
+mol="efz"
+kind="_solv_oct"
 
-mpirun -np 7 sander -O -i minimize.in -o ../libs/1fk9_min_${kind}.out -p ../libs/1fk9_${kind}.prmtop -c ../libs/1fk9_${kind}.rst7  -r ../libs/1fk9_min_${kind}.ncrst
+mpirun -np 2 sander -O -i minimize.in -o ../libs/${mol}_min${kind}.out -p ../libs/${mol}${kind}.prmtop -c ../libs/${mol}${kind}.rst7  -r ../libs/${mol}_min${kind}.ncrst
 conda activate AmberTools21
-ambpdb -p ../libs/1fk9_${kind}.prmtop -c ../libs/1fk9_min_${kind}.ncrst > ../libs/1fk9_min_${kind}.pdb
+ambpdb -p ../libs/${mol}${kind}.prmtop -c ../libs/${mol}_min${kind}.ncrst > ../libs/${mol}_min${kind}.pdb
