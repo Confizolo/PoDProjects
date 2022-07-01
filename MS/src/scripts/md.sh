f31a45f6333d8fe3011ea4a217e4f88a12f05f33
Filippo Conforto <filippo.conforto@outlook.com>
2022-07-01 07:54:36 +0000

    Last outputs

--- MS/src/scripts/md.sh
+++ MS/src/scripts/md.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mol="chain_b_"
+mol="chain_a_efz_"
 kind="solv_box"
 
 
-mpirun -np 4 sander -O -i md_pbc.in -o ../out/${mol}${kind}_md.out -p ../res/${mol}${kind}.prmtop -c ../res/${mol}${kind}_heatpbc.rst  -r ../res/${mol}${kind}_md.rst -x ../res/${mol}${kind}_md.nc
+mpirun -np 8 sander -O -i md_pbc.in -o ../out/${mol}${kind}_md.out -p ../res/${mol}${kind}.prmtop -c ../res/${mol}${kind}_heatpbc.rst  -r ../res/${mol}${kind}_md.rst -x ../res/${mol}${kind}_md.nc
